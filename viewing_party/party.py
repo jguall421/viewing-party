e69907064e1c749007c86fe46f0c1ea7f05db82e
Jesica Guallpa <guallpajesica65@gmail.com>
2025-09-23 13:11:01 -0700

    outline functions & fixed test

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -46,18 +46,33 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    pass
 
 
+def get_most_watched_genre(user_data):
+    pass
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    pass
 
+def get_friends_unique_watched(user_data):
+    pass
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
+def get_available_recs(user_data):
+    pass
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+def get_new_rec_by_genre(user_data):
+    pass
 
+def get_rec_from_favorites(user_data):
+    pass
